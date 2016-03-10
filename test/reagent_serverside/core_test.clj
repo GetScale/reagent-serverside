ea4c6676f8a217abf36b2136f8b52de18137df69
Alex B <me@alexb.io>
2016-03-10 09:12:19 -0800

    add more tests

--- test/reagent_serverside/core_test.clj
+++ test/reagent_serverside/core_test.clj
@@ -1,6 +1,6 @@
 (ns reagent-serverside.core-test
   (:require [clojure.test :refer :all]
-            [reagent-serverside.core :refer :all]
+            [reagent-serverside.core :refer [render-page]]
             [hiccup.core :refer [html]]))
 
 (deftest basic-html
@@ -46,3 +46,26 @@
       (is (= (render-page markup)
              [:div {:data-reactid ".0", :data-react-checksum 890247733}
               [:a {:data-reactid ".0.0"} "First grade math is what?"]])))))
+
+(deftest basic-html-with-classid
+  (testing "single tag with class"
+    (let [markup [:div.class "I have class"]]
+      (is (= (render-page markup)
+             [:div.class {:data-reactid ".0", :data-react-checksum 139399772} "I have class"]))))
+  (testing "single tag with id"
+    (let [markup [:div#id "tag, your id"]]
+      (is (= (render-page markup)
+             [:div#id {:data-reactid ".0", :data-react-checksum -1918824501} "tag, your id"]))))
+  (testing "nested tag with classes"
+    (let [markup [:div.app [:a.github {:href "https://github.com"} "dem g hubs"]]]
+      (is (= (render-page markup)
+             [:div.app {:data-reactid ".0", :data-react-checksum 731064051}
+              [:a.github {:href "https://github.com", :data-reactid ".0.0"} "dem g hubs"]]))))
+  (testing "nested tag with ids"
+    (let [markup [:div#app [:a#company {:href "http://getscale.com"} "mah birthplace"]]]
+      (is (= (render-page markup)
+             [:div#app {:data-reactid ".0", :data-react-checksum -34134260}
+              [:a#company {:href "http://getscale.com", :data-reactid ".0.0"} "mah birthplace"]])))))
+
+;; (deftest html-list-loops)
+;; TODO : for loops with keys for loops
